0cb7569651ae5c37ca100dc4bac916d6f7a42f5c
Ayush Pratap Singh <ayushpratapno1gmail.com>
2025-10-25 00:20:41 +0530

    Fix Static file issue

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -15,15 +15,10 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Media files (uploaded files like images)
-MEDIA_URL = '/media/'  # URL to access media files
+MEDIA_URL = '/media/'  # URL to access media filesAdd commentMore actions
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
@@ -36,10 +31,8 @@ SECRET_KEY = 'django-insecure-u@#5gbc4qu299=zfga_g-kr4z#=+lqq8&&b*3=f1=_by*f*akj
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-#DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-#ALLOWED_HOSTS = ['*']
 
     
 
@@ -132,7 +125,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-# Note: STATIC_URL and STATICFILES_DIRS are already configured above
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
